da13eae1852ee49ae2fb41d31a15467aa1a81341
Victor Fedorov <vpfedorov@bk.ru>
2022-01-05 16:29:45 +0300

    post dweets functionality was added

--- social/views.py
+++ social/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from .models import Profile
+from django.shortcuts import render, redirect
+from .models import Profile, Dweet
+from .forms import DweetForm
 
 
 def dashboard(request):
-    return render(request, "social/dashboard.html")
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("social:dashboard")
+
+    return render(request, "social/dashboard.html", context={"form": form})
 
 
 def profiles(request):
@@ -15,6 +24,7 @@ def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
 
     if request.method == "POST":
+
         current_user_profile = request.user.profile
         data = request.POST
 
